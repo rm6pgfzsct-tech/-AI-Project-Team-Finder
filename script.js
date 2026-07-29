159c8c422b3524d6b9956f54aab1396abd632465
rm6pgfzsct-tech <rm6pgfzsct@privaterelay.appleid.com>
2026-07-29 16:42:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
+function register() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let college = document.getElementById("college").value;
+    let skills = document.getElementById("skills").value;
+    let password = document.getElementById("password").value;
+
+    if (name === "" || email === "" || college === "" || skills === "" || password === "") {
+
+        document.getElementById("message").innerHTML =
+        "Please fill all the fields.";
+
+        return;
+    }
+
+    document.getElementById("message").innerHTML =
+    "Registration Successful!";
+
+}
+
 function login() {
 
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
     if (email === "" || password === "") {
+
         document.getElementById("message").innerHTML =
-        "Please enter your email and password.";
-    } else {
-        document.getElementById("message").innerHTML =
-        "Login successful!";
+        "Please enter email and password.";
+
+        return;
     }
+
+    window.location.href = "dashboard.html";
 
 }
