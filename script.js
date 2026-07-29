e8fbd0618f3fe37b7a05d25feb9f245fdc6378bc
rm6pgfzsct-tech <rm6pgfzsct@privaterelay.appleid.com>
2026-07-29 16:35:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    alert("Welcome to AI Project Team Finder!");
-});
+function login() {
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (email === "" || password === "") {
+        document.getElementById("message").innerHTML =
+        "Please enter your email and password.";
+    } else {
+        document.getElementById("message").innerHTML =
+        "Login successful!";
+    }
+
+}
